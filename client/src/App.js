17d66735890bd6853e2c772fecf759dd8536a3c4
Anas Tazir <atazir2018@gmail.com>
2021-08-31 19:33:05 +0530

    make a folder for compontents

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { Chat } from 'stream-chat-react'
 import {StreamChat} from 'stream-chat'
 import Cookies from 'universal-cookie'
 
+import ChannelContainer from './components/ChannelContainer';
+import ChannelListContainer from './components/ChannelListContainer';
+
 const apiKey = 'YOURAPIKEY'
 
 const client= StreamChat.getInstance(apiKey)
@@ -11,7 +14,12 @@ function App() {
   return (
     <div className="App">
         <Chat client={client} theme= 'team light'>
-
+            <ChannelListContainer
+            
+            />
+            <ChannelContainer
+            
+            />
         </Chat>
     </div>
   );
