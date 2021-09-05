9773f083a7655520bc781bf96e96e18c378b424c
Anas Tazir <atazir2018@gmail.com>
2021-09-05 12:09:17 +0530

    add side bar

--- client/src/components/ChannelListContainer.js
+++ client/src/components/ChannelListContainer.js
@@ -1,12 +1,31 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import {ChannelList, useChatContext} from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
+import HospitalIcon from '../assets/hospital.png'
+import LogoutIcon from '../assets/logout.png'
+
+const Sidebar = () => (
+    <div className="channel-list__sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div className="icon1__inner">
+                <img src={HospitalIcon} alt="Hospital" width="30" />
+            </div>
+        </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner">
+                <img src={LogoutIcon} alt="Logout" width="30" />
+            </div>
+        </div>
+    </div>
+)
+
 const ChannelListContainer = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Sidebar />
+        </>
     )
 }
 
